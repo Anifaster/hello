49582f83549d5cd869eedf11b70b38f7799a7082
Anifaster <uzumakiisfandiyor@gamil.com>
2022-07-20 06:03:00 +0300

    boldi

--- src/menu/Navbar.jsx
+++ src/menu/Navbar.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { NavLink, Route, Routes } from 'react-router-dom';
+import { Link, NavLink, Route, Routes } from 'react-router-dom';
 import Chefs from './Chefs';
 import Shop from './Shop';
-import Logo from './logo.png'
+import Logo from './logo.png';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper";
 const Navbar = () => {
-    return ( 
+    return (
         <div>
             <div className='navbar'>
                 <div className="logo">
-                <img className='img' src={Logo}/>
+                    <img className='img' src={Logo} />
                 </div>
-                
+
                 <div className="menu">
                     {/* <select name="select" id="select" className='select'>
                         <option value="" className='option'>Home page </option>
@@ -18,7 +22,7 @@ const Navbar = () => {
                         <option value="">Home page 3</option>
                         <option value="">Home page 4</option>
                     </select> */}
-                    <NavLink  to="/Chefs" className="nav-link">Chefs</NavLink>
+                    <NavLink to="/Chefs" className="nav-link">Chefs</NavLink>
                     <NavLink to='/Shop' className="nav-link">Shop</NavLink>
                     <NavLink to='/' className="nav-link">Home</NavLink>
                     <select className='select2'>
@@ -30,11 +34,28 @@ const Navbar = () => {
                 </div>
             </div>
             <Routes>
-                <Route path='/chefs' element={<Chefs/>} />
-                <Route path='/shop' element={<Shop/>}/>
+                <Route path='/chefs' element={<Chefs />} />
+                <Route path='/shop' element={<Shop />} />
             </Routes>
+            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+                <SwiperSlide>
+                    <div className='slide1'>
+                        <h1>
+                            Hot eats, cold treats.
+                        </h1>
+                        <p>
+                            There are some things money can't buy. For everything else, there's Mastercards
+                           
+                        </p>
+                        <p>
+                        You deserve a break today.Wake up with the King. The flavors of nature Just wow!
+                        </p>
+                        <Link to='/' className='view' >View our menu</Link>
+                    </div>
+                </SwiperSlide>
+            </Swiper>
         </div>
-     );
+    );
 }
- 
+
 export default Navbar;
