c6e886a4dec4ddcd07f6d86134b3a2eac082acae
jonykilian <jonykilian@gmail.com>
2022-07-23 16:27:51 +0500

    cff

--- src/menu/Chefs.jsx
+++ src/menu/Chefs.jsx
@@ -1,8 +1,55 @@
 import React from 'react';
+import { NavLink, Route, Routes } from 'react-router-dom';
+import Shop from './Shop';
 const Chefs = () => {
     return ( 
         <div>
-            <h1>Chefs</h1>
+            <NavLink className='btn-menu' to='/shop'>Go to menu</NavLink>
+            <div className="chefs">
+                <div className="chef">
+                    <img src="	https://jthemes.net/themes/html/fodige/assets/img/chefs/ch1.jpg" alt="" />
+                    <h1>Lukaku</h1>
+                    <h2>He has been working in our company for 3 years</h2>
+                    <p>
+                        <NavLink to=''>"Burgers"</NavLink>
+                        <NavLink to="">"Pizza"</NavLink>
+                        <NavLink to=''>"Hot-Dog"</NavLink>
+                    </p>
+                </div>
+                <div className="chef">
+                    <img src="https://jthemes.net/themes/html/fodige/assets/img/chefs/ch2.jpg" alt="" />
+                    <h1>Tomson</h1>
+                    <h2>He has been working in our company for 5 years</h2>
+                    <p>
+                        <NavLink to=''>"Burgers"</NavLink>
+                        <NavLink to="">"Pizza"</NavLink>
+                        <NavLink to=''>"Hot-Dog"</NavLink>
+                    </p>
+                </div>
+                <div className="chef">
+                    <img src="https://jthemes.net/themes/html/fodige/assets/img/chefs/ch4.jpg" alt="" />
+                    <h1>Alihon</h1>
+                    <h2>He has been working in our company for 10 years</h2>
+                    <p>
+                        <NavLink to=''>"Burgers"</NavLink>
+                        <NavLink to="">"Pizza"</NavLink>
+                        <NavLink to=''>"Hot-Dog"</NavLink>
+                    </p>
+                </div>
+                <div className="chef">
+                    <img src="https://jthemes.net/themes/html/fodige/assets/img/chefs/ch3.jpg" alt="" />
+                    <h1>Islomhon</h1>
+                    <h2>He has been working in our company for 1 years</h2>
+                    <p>
+                        <NavLink to=''>"Burgers"</NavLink>
+                        <NavLink to="">"Pizza"</NavLink>
+                        <NavLink to=''>"Hot-Dog"</NavLink>
+                    </p>
+                </div>
+            </div>
+            <Routes>
+                <Route path='/shop' element={<Shop/>} />        
+            </Routes>
         </div>
      );
 }
---
+++ src/menu/chefs/chef1.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Chef1 = () => {
+    return ( 
+        <div>
+            
+        </div>
+     );
+}
+ 
+export default Chef1;
